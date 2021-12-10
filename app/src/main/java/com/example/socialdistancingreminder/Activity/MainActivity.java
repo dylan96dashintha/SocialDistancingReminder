1bc64a593da979f8f08dc25739f2ff459e637b1e
Dilan <djdilan09@gmail.com>
2021-12-10 14:55:34 +0530

    carousal slider

--- app/src/main/java/com/example/socialdistancingreminder/Activity/MainActivity.java
+++ app/src/main/java/com/example/socialdistancingreminder/Activity/MainActivity.java
@@ -14,9 +14,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.socialdistancingreminder.Model.BluetoothScan.BluetoothScan;
+import com.example.socialdistancingreminder.Model.SliderData.SliderAdapter;
+import com.example.socialdistancingreminder.Model.SliderData.SliderData;
 import com.example.socialdistancingreminder.R;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Slider urls
+    String url1 = "https://www.health.nsw.gov.au/Infectious/covid-19/PublishingImages/wear-a-mask-t-sinhalese.png";
+    String url2 = "https://www.health.nsw.gov.au/Infectious/covid-19/PublishingImages/Domestic-cleaning-social-TW-Sinhalese.jpg";
+    String url3 = "https://www.health.nsw.gov.au/Infectious/covid-19/PublishingImages/no-visitors-sinhalese-tt.jpg";
+
+
     private static final String TAG = "MainActivity";
     public BluetoothScan blueScan;
     Button pulicPlaceButton;
@@ -32,6 +44,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        //slider logic
+        // we are creating array list for storing our image urls.
+        ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
+
+        // initializing the slider view.
+        SliderView sliderView = findViewById(R.id.slider);
+
+        // adding the urls inside array list
+        sliderDataArrayList.add(new SliderData(url1));
+        sliderDataArrayList.add(new SliderData(url2));
+        sliderDataArrayList.add(new SliderData(url3));
+
+        // passing this array list inside our adapter class.
+        SliderAdapter adapter = new SliderAdapter(this, sliderDataArrayList);
+
+        // below method is used to set auto cycle direction in left to
+        // right direction you can change according to requirement.
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+
+        // below method is used to
+        // setadapter to sliderview.
+        sliderView.setSliderAdapter(adapter);
+
+        // below method is use to set
+        // scroll time in seconds.
+        sliderView.setScrollTimeInSec(3);
+
+        // to set it scrollable automatically
+        // we use below method.
+        sliderView.setAutoCycle(true);
+
+        // to start autocycle below method is used.
+        sliderView.startAutoCycle();
+        //
         int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
         permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
         if (permissionCheck != 0) {
